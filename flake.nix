b63c4ca6894e30e3800b4ebe593503011b27a95e
pillowtrucker <githubino@janitor.chat>
2024-03-15 00:22:53 +0100

    ok 9.0.2 seems to be working probably

--- flake.nix
+++ flake.nix
@@ -3,19 +3,22 @@
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+  inputs.haskus-packages.url = "github:pillowtrucker/haskus-packages/mine";
+  outputs = { self, nixpkgs, flake-utils, haskellNix, haskus-packages }:
     let
       supportedSystems = [
         "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
+#        "x86_64-darwin"
+#        "aarch64-linux"
+#        "aarch64-darwin"
       ];
     in
       flake-utils.lib.eachSystem supportedSystems (system:
       let
         overlays = [ haskellNix.overlay
+        
           (final: prev: {
+            
             hixProject =
               final.haskell-nix.hix.project {
                 src = ./.;
@@ -28,7 +31,7 @@
       in flake // {
         legacyPackages = pkgs;
 
-        packages.default = flake.packages."hello:exe:hello";
+#        packages.default = flake.packages."hello:exe:hello";
       });
 
   # --- Flake Local Nix Configuration ----------------------------
--- nix/hix.nix
+++ nix/hix.nix
@@ -1,9 +1,9 @@
 {pkgs, ...}: {
   # name = "project-name";
-  compiler-nix-name = "ghc981"; # Version of GHC to use
+  compiler-nix-name = "ghc902"; # Version of GHC to use
 
   crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
-    p.mingwW64
+#    p.mingwW64
     # p.ghcjs # TODO GHCJS support for GHC 9.2
   ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
     p.musl64
@@ -11,6 +11,8 @@
 
   # Tools to include in the development shell
   shell.tools.cabal = "latest";
-  shell.tools.hlint = "latest";
-  shell.tools.haskell-language-server = "latest";
+#  shell.tools.hlint = "3.4";
+#  shell.tools.haskell-language-server="latest";
+  shell.tools.hlint = "3.3.6";
+  shell.tools.haskell-language-server = "2.4.0.0";
 }
